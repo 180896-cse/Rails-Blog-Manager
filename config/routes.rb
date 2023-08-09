a3109ef247037cfbe94c72b67e155be7704f4f6a
180896-cse <shantanupratap180896@gmail.com>
2023-08-09 23:01:49 +0530

    worked with sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
   resources :posts
+
+  get '/register', to: 'users#new'
+  resources :users, only: [:create]
+
+  get '/sign_in', to: 'sessions#new'
+  get '/sign_out', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
