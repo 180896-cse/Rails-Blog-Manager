d6053d75843f082b8bf023d48cbd007b0d3023ff
180896-cse <shantanupratap180896@gmail.com>
2023-08-09 20:54:55 +0530

    added user part in app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_09_144015) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_09_152307) do
   create_table "comments", force: :cascade do |t|
     t.text "content", null: false
     t.integer "post_id", null: false
@@ -24,6 +24,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_144015) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
